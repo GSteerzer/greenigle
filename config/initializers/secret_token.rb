e86f3138669f273cbec2ec48b0c54bdb6d5ae4b7
Grant M. Steerzer <GSteerzer@gmail.com>
2013-03-21 14:43:09 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenigle::Application.config.secret_token = '31ac252eac59d6c62feba50e712fbbb3243d0a7fc221971d8f53d0eb400503535c43b34d4cb3befc5c8daa295ad2c4145470ccef0886769bc86f2f0404c12f92'
